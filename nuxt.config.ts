4329d6e23a4fae05c146e11bead7fca908796a21
msavulescu <msavulescu@wallbid.io>
2024-03-17 16:02:21 +0200

    Added firebase config & basic pages.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,28 @@ import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  typescript: { shim: false },
+  typescript: {
+    tsConfig: {
+      extends: "../../../tsconfig.base.json",
+    },
+  },
+
   imports: {
     autoImport: true,
   },
 
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-vuefire"],
+  vuefire: {
+    auth: {
+      enabled: false,
+    },
+    config: {
+      apiKey: process.env.apiKey,
+      authDomain: process.env.authDomain,
+      projectId: process.env.projectId,
+      storageBucket: process.env.storageBucket,
+      messagingSenderId: process.env.messagingSenderId,
+      appId: process.env.appId,
+    },
+  },
 });
--- pages/Login.vue
+++ pages/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>App vue</h1>
+    <h1>Login Page</h1>
   </div>
 </template>
 
